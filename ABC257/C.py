7e6ac8fe061e98cd97b42d9caa7f97e87f0b0dc7
isshy-you <isshy@kit.hi-ho.ne.jp>
2022-06-27 00:23:43 +0900

    * not completed(ACx31,WAx4)

--- ABC257/C.py
+++ ABC257/C.py
@@ -1,52 +1,52 @@
 # -*- coding: utf-8 -*-
-# AC  x5
-# TLE x28
-# RE  x2
+# AC  x31
+# WA  x4
 import sys
-sys.setrecursionlimit(1000000)
 
+DEBUG=0
 N = int(sys.stdin.readline())
 S = sys.stdin.readline()
 str = sys.stdin.readline().split()
 
 W=[]
+tuples = []
+sum=0
 for ii in range(N):
     W.append(int(str[ii]))
+    s=int(S[ii])
+    tuples.append((s,W[ii]))
+    sum=sum+int(s)
 
-adult = []
-child = []
-num = 0
-# for ii in range(N):
-#     num = num + int(S[ii])
+if sum==N:
+    print(3)
+    exit(0)
+if sum==0:
+    print(3)
+    exit(0)
 
-for ii in range(N):
-    if S[ii]=='1':
-        adult.append(W[ii])
+# print(tuples)
+tuples_sorted = sorted(tuples, key=lambda aa: aa[1])
+if (DEBUG==1):print(tuples_sorted)
+
+nummax=sum
+flag,last=tuples_sorted[0]
+adult=sum
+child=0
+adult_l=adult
+child_l=child
+for ii in range(1,N,1):
+    flag1,weight1=tuples_sorted[ii-1]
+    if flag1 == 0:
+        child = child + 1
     else:
-        child.append(W[ii])
-adult.sort()
-child.sort()
-adultn = len(adult)
-childn = len(child)
-
-# print(adult)
-# print(child)
-nummax = 0
-if len(adult)==0:
-    nummax = N
-elif len(child)==0 and child[childn-1]!=10**9:
-    nummax = N
-else:
-    for ii in range(adultn):
-        for jj in range(childn):
-            # print('adult[ii],child[jj]=',ii,jj,adult[ii],child[jj])
-            if adult[ii] > child[jj]:
-                num = adultn-ii + jj +1
-                # print('adult',num)
-            elif adult[ii] <= child[jj]:
-                num = adultn-ii-1 + jj
-                # print('child',num)
-            if num >= nummax:
-                nummax = num
-
+        adult = adult - 1
+    if last!=weight1:
+        if nummax<(child_l+adult_l):
+            nummax = child_l+adult_l
+        if (DEBUG==1):print(ii-1,child_l,adult_l,nummax,'*')
+    else:
+        if (DEBUG==1):print(ii-1,child_l,adult_l,nummax)
+    last = weight1
+    adult_l=adult
+    child_l=child
 print(nummax)
