726098d0b16e1788f3558e56910c2c2af9fc4140
isshy-you <85348572+isshy-you@users.noreply.github.com>
2022-07-10 03:21:30 +0900

    * all AC

--- ABC259/C.py
+++ ABC259/C.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
-# ACx28
-# WAx4
+# ACx32
+
 import sys
 import math
 
 sys.setrecursionlimit(1000000)
-
-DEBUG=0
 
 #input
 # n,x = sys.stdin.readline().split()
@@ -21,49 +19,91 @@ DEBUG=0
 S = sys.stdin.readline()
 T = sys.stdin.readline()
 
+DEBUG=0
+
 if DEBUG==1:
-    # print('{}N歳のとき{}Tcmになった'.format(N,T))
-    # print('{}M歳の身長cmは？'.format(M))
-    # print('{}X歳まで身長が毎年{}Dcm伸びた'.format(X,D))
-    print('S:',S[:-1])
-    print('T:',T[:-1])
-start=[]
-goal=[]
-if S[0]==T[0] and S[1]==T[1]: #最初は同じなら
-    flag = 0
-    sp = 2
-    for tp,ss in enumerate(T[:-1]):
-        if tp>=2:
-            if S[sp-1]==S[sp-2]:#同じ文字が続いたら
-                flag = 1
-                if DEBUG==1:print('flagON',sp,tp,S[sp],T[tp])
-            else:
-                flag = 0
-                if DEBUG==1:print('flagOFF',sp,tp,S[sp],T[tp])
-            if S[sp]!=T[tp]: #不一致なら
-                if DEBUG==1:print(S[sp],T[tp])
-                if sp==len(S)-2 and S[sp]!=T[tp+1]: #Sが最後の文字だったらNo
-                    if DEBUG==1:print('last sp is differ')
-                    print('No')
-                    sys.exit()
-                if flag!=1: #同じ文字が続いてなかったらNo
-                    if DEBUG==1:print('flag',flag)
-                    print('No')
-                    sys.exit()
-                if (sp>tp): #ポインタが逆転したら（効果なし？）
-                    print('No')
-                    sys.exit()
-            else: #文字が同じなら次の文字へ
-                if (sp < len(S)-2):
-                    if DEBUG==1:print('sp++')
-                    sp = sp + 1
-
-        # if DEBUG==1:
-        #     print('sp,tp,ss',sp-1,tp,ss)
-
-else:
+    print('S:',S[:-1],len(S)-1)
+    print('T:',T[:-1],len(T)-1)
+lens=len(S)-1
+lent=len(T)-1
+if lens>lent: # TはS以上の文字数であること
+    if DEBUG==1:print('len(S)>len(T)')
     print('No')
     sys.exit()
-print('Yes')    
+elif S[0]!=T[0]: #最初1文字が同じであること
+    if DEBUG==1:print('S[0]!=T[0]')
+    print('No')
+    sys.exit()
+elif S[1]!=T[1]: #最初の2文字が同じであること
+    if DEBUG==1:print('S[1]!=T[1]')
+    print('No')
+    sys.exit()
+else:
+    flag = 0
+    sp = 2 #2つ目から開始
+    tp = 2
+    while (sp <= lens) and (tp < lent):
+        if DEBUG==1:print('sp,tp,S,P',sp,tp,S[sp],T[tp])
+        #Sの1つ前と2つ前が同じ文字ならフラグを立てる
+        if S[sp-1]==S[sp-2]:
+            flag = 1
+            if DEBUG==1:print('flagON')
+        else:
+            flag = 0
+            if DEBUG==1:print('flagOFF')
+        # SとPのそれぞれのポインタの文字が不一致で
+        # Pが直前と同じ文字で
+        # 　flag=1ならTのポインタだけを進める(Sに文字挿入)
+        if S[sp]!=T[tp]: #不一致なら
+            if DEBUG==1:print('comp',S[sp],T[tp])
+            if T[tp]==T[tp-1]:
+                if flag==1:
+                    if DEBUG==1:print('tp++')
+                    tp=tp+1
+                else: #同じ文字が続いてなかったらNo
+                    if DEBUG==1:print('flag=0')
+                    print('No')
+                    sys.exit()
+            else:
+                if DEBUG==1:print('TP[tp]!=TP[tp-1]')
+                print('No')
+                sys.exit()
+        # SとPのそれぞれのポインタの文字が一致すれば、
+        # 　SがPを追い越さなければ
+        # 　　SとPのポインタを進める。
+        #   SがPを追い越すのならNo
+        # 　Sが最後の文字ならYes
+        else: # 一致なら
+            if (sp < lens): # spは最後じゃない
+                if DEBUG==1:print('sp++,tp++')
+                sp = sp + 1
+                tp = tp + 1
+            else:# Sが最後の文字なら
+                if tp==lent-1: # Tが最後ならYES
+                    if DEBUG==1:print('last S')
+                    print('Yes')
+                    sys.exit()
+                else:
+                    if DEBUG==1:print('last S')
+                    print('No')
+                    sys.exit()
+
+if DEBUG==1:print('sp,tp,S,P',sp,tp,S[sp],T[tp])
+if (tp==lent):
+    print('Yes')
+else:
+    print('No')
 
 sys.exit()
+
+#YES
+# abbaac
+# abbbbaaac
+
+#YES
+# aabbcc
+# aabbccc
+
+#NO
+# aabbcc
+# aabbccd
