fd4e4f29caec314369cba80df6c4e86722082a19
isshy-you <85348572+isshy-you@users.noreply.github.com>
2022-07-24 22:19:30 +0900

    * ABC261,C,All-AC

--- ABC261/C.py
+++ ABC261/C.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-# pypy3 TLEx2 ACx14 ... use this
-# pytho3.8 TLEx5 ACx11
+# use dict
+# pypy3 ACx16
+# pytho3.8 ACx16
 
 import sys
 
@@ -9,8 +10,7 @@ DEBUG=False
 N = int(input())
 
 num=1
-lib=[]
-cnt=[]
+lib={}
 for kk in range(N): # N文字入力
     text = sys.stdin.readline()[:-1] #文字列入力
     tmpstr='' # 空白文字で初期化
@@ -18,24 +18,20 @@ for kk in range(N): # N文字入力
         tmpstr = tmpstr + str(ord(text[mm])-96).zfill(2) # a~zを01~26に変換
     code = int(tmpstr) #文字列の数値化
     if kk==0: # 最初だけ初期化
-        lib.append(code) # 辞書追加
-        cnt.append(0)    # 辞書カウンタ0
+        lib.setdefault(code,0) # 辞書追加
         print(text)      # 結果表示
     else:
-        result = False
-        for nn in range(num): # 辞書をなめる
-            if (DEBUG):print('compare:',nn,lib[nn],code)
-            if lib[nn]==code: # 辞書にあれば（数値比較で比較１回となる）
-                cnt[nn]= cnt[nn] + 1 # 辞書カウントアップ
-                print(text+'('+str(cnt[nn])+')') # 一致結果表示
-                if (DEBUG):print('update:',lib,cnt)
-                result = True
-                break # 一致したら辞書ループを終わる
-            # 辞書になかったら
-        if not result:
+        # result = False
+        # if (DEBUG):print('compare:',key,code,cnt)
+        if code in lib: # 辞書にあれば（数値比較で比較１回となる）
+            lib[code] = lib[code]+1 # 辞書カウントアップ
+            print(text+'('+str(lib[code])+')') # 一致結果表示
+            # if (DEBUG):print('update:',key,lib[code])
+            # result = True
+            # break # 一致したら辞書ループを終わる
+        else:
             num = num + 1    # 辞書数増加
-            lib.append(code) # 辞書追加
-            cnt.append(0)    # 辞書カウンタ0
+            lib[code]=0 # 辞書追加
             print(text) # 不一致結果表示
-            if (DEBUG):print('append:',lib,cnt)
+            # if (DEBUG):print('append:',key,lib[code])
 sys.exit()
